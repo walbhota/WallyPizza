ed5458e8fdc0625622d57182d40ed38471b76e8a
j8 <walbhota@gmail.com>
2022-07-08 17:38:29 +0100

    Added the HTTP verbs

---
+++ Models/Pizza.cs
@@ -0,0 +1,11 @@
+namespace WallyPizza.Models;
+
+        public class Pizza
+{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+}
+
+
+
